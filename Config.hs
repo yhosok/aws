690c8ef0a48faf192e2e8e4237707b055060e265
Yasunori Hosokawa <goroii@gmail.com>
2012-03-12 00:29:16 +0000

    change lib for yaml to Data.Yaml

--- Config.hs
+++ Config.hs
@@ -2,12 +2,12 @@
 module Config where
 
 import Network.HTTP (RequestMethod (..))
-import Data.Object.Yaml
-import Data.Object
+import Data.Yaml
 import Data.ByteString.Char8 (ByteString,pack)
 import Data.Text (Text)
 import Data.Text.Encoding (decodeUtf8)
 import Control.Monad (join)
+import Control.Applicative
 import Network.HTTP.Types (QueryText)
 
 data AWSConfig = AWSConfig { secKey :: ByteString
@@ -26,12 +26,17 @@ mkCommonP accKey = [("Version",Just "2011-12-15"),
 
 loadConf :: IO AWSConfig
 loadConf = do
-  obj <- join $ decodeFile "config.yml"
-  confs <- fromMapping obj
-  sec <- lookupScalar ("secKey"::String) confs
-  acc <- lookupScalar "accKey" confs
-  h <-  lookupScalar "host" confs
-  p <- lookupScalar "path" confs
+  mobj <- decodeFile "config.yml"
+  case mobj of
+    Just (Object obj) -> parseMonad parseConfig obj
+    _ -> fail $ "can not find file : config.yml"
+
+parseConfig :: Object -> Parser AWSConfig
+parseConfig o = do
+  sec <- o .: "secKey"
+  acc <- o .: "accKey"
+  h <- o .: "host"
+  p <- o .: "path"
   return $ AWSConfig
     { secKey = sec
     , accKey = acc
@@ -40,3 +45,4 @@ loadConf = do
     , path = p
     , commonParam = mkCommonP acc
     }
+
